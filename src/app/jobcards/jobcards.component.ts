e095ef1a6c55ee24e2aa90f5ff362d075d8940e1
shrey_akan <shereyansjain0904@gmail.com>
2024-01-09 21:57:59 -0800

    update

--- src/app/jobcards/jobcards.component.ts
+++ src/app/jobcards/jobcards.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../auth/user.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 interface Job {
   jobtitle: string;
@@ -17,15 +20,71 @@ interface Job {
   styleUrls: ['./jobcards.component.css']
 })
 
-export class JobcardsComponent {
-  public job: Job = {
-    jobtitle: "Software Developer",
-    companyforthisjob: "Orage Technologies",
-    numberofopening: "12",
-    locationjob: "Noida, Uttar Pradesh",
-    descriptiondata: ["meow", "sekiro"],
-    jobtype: "Remote",
-    schedulejob: "Day Shift",
-    payjob: "25000 - 45000",
+export class JobcardsComponent implements OnInit {
+  data1: any;
+  companies = [
+    // ... existing companies data
+  ];
+  searchQuery: string = '';
+  showFooter = true;
+  showJobFeed = true;
+  showJobSearches = false;
+  selectedJob: Job | null = null;
+  data: Job[] = [];
+  itemsPerPage = 5;
+  currentPage = 1;
+  totalPages!: number;
+
+  constructor(private router: Router, private b1: UserService) {}
+
+  performSearch() {
+    // Implement search logic if needed
   }
+
+  onPageChange(page: number): void {
+    this.currentPage = page;
+  }
+
+  ngOnInit(): void {
+    let response = this.b1.fetchjobpost();
+    response.subscribe((data1: any) => {
+      this.data1 = data1;
+      this.data = data1;
+      this.totalPages = Math.ceil(this.data.length / this.itemsPerPage);
+    });
+
+  }
+
+  searchJobs() {
+    this.data = this.data1.filter((job: Job) => {
+      const titleMatch = job.jobtitle.toLowerCase().includes(this.searchQuery.toLowerCase());
+      const locationMatch = job.locationjob.toLowerCase().includes(this.searchQuery.toLowerCase());
+      return titleMatch || locationMatch;
+    });
+  }
+
+  navigateToSignIn() {
+    this.router.navigate(['/login']);
+  }
+
+  navigateToSignUp() {
+    this.router.navigate(['/register']);
+  }
+
+
+  getJobsForCurrentPage(): Job[] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.data.slice(startIndex, endIndex);
+  }
+  // public job: Job = {
+  //   jobtitle: "Software Developer",
+  //   companyforthisjob: "Orage Technologies",
+  //   numberofopening: "12",
+  //   locationjob: "Noida, Uttar Pradesh",
+  //   descriptiondata: ["meow", "sekiro"],
+  //   jobtype: "Remote",
+  //   schedulejob: "Day Shift",
+  //   payjob: "25000 - 45000",
+  // }
 }
