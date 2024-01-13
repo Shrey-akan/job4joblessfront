33e34a205269f0e1554dc2e4ef9071497f6cb223
shrey_akan <shereyansjain0904@gmail.com>
2024-01-12 22:25:56 -0800

    update privacy page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -72,7 +72,7 @@ const routes: Routes = [
     canActivate: [authGuard]
   },
   {
-    path:'privacypolicy/' , component:PrivacypolicyComponent
+    path:'privacypolicy' , component:PrivacypolicyComponent
   },
   {
     path: 'dashboardemp',
