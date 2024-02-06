f6a6f66ebbeaf2ec9f28fa8dd13500efe653248a
shrey_akan <shereyansjain0904@gmail.com>
2024-02-06 17:42:38 +0530

    updated firebase auth

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyB3qyfF9Dxaqg2mW2hy5LkHRINhSv8V8ZA',
-    authDomain: 'hawkjob-29010.firebaseapp.com',
-    projectId: 'hawkjob-29010',
-    storageBucket: 'hawkjob-29010.appspot.com',
-    messagingSenderId: '889757152232',
-    appId: '1:889757152232:web:31651a407764be03aaaa5d',
-    measurementId: 'G-LYN6V264TL',
+    apiKey: "AIzaSyDw9hdPbfmmlm22NmPxXg4s5mFd-ky0hGE",
+    authDomain: "job4jobless-c7aa9.firebaseapp.com",
+    projectId: "job4jobless-c7aa9",
+    storageBucket: "job4jobless-c7aa9.appspot.com",
+    messagingSenderId: "935423765278",
+    appId: "1:935423765278:web:76fdc14021fe0be187b70f",
+    measurementId: "G-WCE9812CJY",
     vapidKey:
-      'BFDKJ5p5-HZhOY7zC2vde_pDytahoTIfoqs4ayCR8CSYWP-Jym3Vz2XP4E5yDjxqiD5pLST1PFMLESwaVrGB1IU',
+      'BPSGo0nFPKdavYW4OEYdEviXu2N0jr1M-1ztjTAPSVne11-AmzpL-CGLPNQj5BgXrQvT8ClX9eOsOQaZkvPkLZY',
   },
 };
