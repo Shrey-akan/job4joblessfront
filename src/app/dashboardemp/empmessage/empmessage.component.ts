f5be2201e595a8e1fe2f52f68f4cdf438d6ff26e
shrey_akan <shereyansjain0904@gmail.com>
2024-02-20 11:48:38 +0530

    as

--- src/app/dashboardemp/empmessage/empmessage.component.ts
+++ src/app/dashboardemp/empmessage/empmessage.component.ts
@@ -43,7 +43,7 @@ export class EmpmessageComponent implements OnInit {
 
     initSocketConnection() {
         // Connect to the Socket.IO server using HTTPS
-        this.socket = io('http://164.92.121.188:4444');
+        this.socket = io('https://rocknwoods.website:4444');
 
         // Event: Socket Error
         this.socket.on('error', (error: any) => {
