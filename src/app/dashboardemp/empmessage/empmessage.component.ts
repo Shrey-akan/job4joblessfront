e9a239d5b1e2f96dd93a3cd6fbeb252e384ad6aa
shrey_akan <shereyansjain0904@gmail.com>
2024-02-21 02:56:56 +0530

    k

--- src/app/dashboardemp/empmessage/empmessage.component.ts
+++ src/app/dashboardemp/empmessage/empmessage.component.ts
@@ -33,12 +33,16 @@ export class EmpmessageComponent implements OnInit {
       message: ['', Validators.required]
     });
 
+    // Retrieve empid from cookie and uid from route parameters
     this.empid = this.cookie.get('emp');
     this.uid = this.route.snapshot.paramMap.get("uid");
+
+    // Initialize the socket connection
     this.initSocketConnection();
   }
 
   ngOnInit(): void {
+    // Fetch previous messages on component initialization
     this.fetchMessages();
   }
 
@@ -48,8 +52,8 @@ export class EmpmessageComponent implements OnInit {
       transports: ['websocket'],
       autoConnect: false,
       query: {
-        empid: this.empid,
-        uid: this.uid
+        sourceId: this.empid,
+        targetId: this.uid
       }
     });
 
@@ -63,22 +67,23 @@ export class EmpmessageComponent implements OnInit {
 
     // Event: Receive message
     this.socket.on('message', (message: SendMessage) => {
-      console.log('Received message:', message); // Log received message
+      console.log('Received message:', message);
       // Add received message to the messages array
       this.messages.push(message);
     });
-}
-
+  }
 
   fetchMessages() {
     // Fetch previous messages between the current user (empid) and the target user (uid)
     this.http.get<SendMessage[]>('https://job4jobless.com:9001/fetchMessages').subscribe((messages: SendMessage[]) => {
+      // Filter messages to include only those related to the current conversation
       this.messages = messages.filter(
         (message) =>
           (message.messageTo === this.uid && message.messageFrom === this.empid) ||
           (message.messageTo === this.empid && message.messageFrom === this.uid)
       );
 
+      // If there are previous messages, display the last message in the form
       if (this.messages.length > 0) {
         this.messageForm.patchValue({
           message: this.messages[this.messages.length - 1].message,
@@ -90,22 +95,26 @@ export class EmpmessageComponent implements OnInit {
   sendMessage() {
     if (this.messageForm.valid && this.uid) {
       const messageToSend = {
-        messageTo: this.uid, // Set uid as the target ID
-        messageFrom: this.empid, // Set empid as the source ID
+        messageTo: this.uid,
+        messageFrom: this.empid,
         message: this.messageForm.value.message
       };
 
-      console.log('Sending message:', messageToSend); // Log sending message
+      console.log('Sending message:', messageToSend);
 
-      this.socket.emit('message', messageToSend); // Send message via Socket.IO
+      // Send the message via Socket.IO
+      this.socket.emit('message', messageToSend);
       
+      // Send the message to the server API as well
       this.http.post<SendMessage>('https://job4jobless.com:9001/send', messageToSend).subscribe({
         next: (response: any) => {
-          console.log('API Response:', response); // Log API response
+          console.log('API Response:', response);
+          // Clear the message input after sending
           this.messageForm.patchValue({
-            message: '', // Clear the message input after sending
+            message: '',
           });
-          this.fetchMessages(); // Fetch messages again to update the view
+          // Fetch messages again to update the view
+          this.fetchMessages();
         },
         error: (err: any) => {
           console.error('Error sending message:', err);
