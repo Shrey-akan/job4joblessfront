261b6e770beb15511f0a810b927bf5779a431855
shrey_akan <shereyansjain0904@gmail.com>
2024-03-14 09:47:18 +0530

    14 march

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -1,3 +1,3 @@
-// export const backendUrl = 'https://job4jobless.com:9001/';
-export const backendUrl = 'http://localhost:9001/';
+export const backendUrl = 'https://job4jobless.com:9001/';
+// export const backendUrl = 'http://localhost:9001/';
 export const OtpUrl = 'https://rocknwoods.website:3000/0auth/';
